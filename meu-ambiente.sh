782224d3336bc8f33b381d0ef44fa7375cd63e7d
Jurandy Soares <jurandy.soares@gmail.com>
2022-10-24 07:46:19 -0300

    feat: Configuração de variáveis de ambiente para o curso

---
+++ meu-ambiente.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/zsh
+
+# Variáveis globais
+
+declare -A var_dysconta
+
+obtem_nome_sobrenome(){
+  unset 'NOME'
+  unset 'SOBRENOME'
+
+  nome_completo=($(getent passwd "${USER}" | cut -d: -f5 | cut -d, -f1))
+  NOME="${nome_completo[1]}"
+  sobrenomes=${nome_completo[2,-1]}
+
+  preposicoes=(de da das "do" dos e)
+  sobrenomes_sem_preps=${sobrenomes}
+
+  for prep in "${preposicoes[@]}"; do
+      sobrenomes_sem_preps="${sobrenomes_sem_preps// ${prep} / }"
+  done
+
+  palavras=(${sobrenomes_sem_preps})
+  num_palavras=${#palavras[@]}
+
+  PS3="Olá, ${prim_nome}!
+Por gentileza, informe o número de seu sobrenome preferido: "
+
+  if [[ ${#palavras} -eq 1 ]]; then
+    SOBRENOME="${palavras[1]}"
+  elif [[ ${#palavras} -eq 0 ]]; then
+    exit
+  else
+    select SOBRENOME in "${palavras[@]}"; do
+
+        if [[ "${REPLY}" -gt 0 ]] && [[ "${REPLY}" -le "${#palavras}" ]]; then
+          break
+        fi
+    done
+  fi
+
+  if ! [ -v APELIDO ]; then
+    print "Olá, ${NOME}! Como você gostaria de ser chamado?"
+    read APELIDO
+    var_dysconta[APELIDO]="${APELIDO}"
+  fi
+
+  var_dysconta[NOME]="${NOME}"
+  var_dysconta[SOBRENOME]="${SOBRENOME}"
+}
+
+
+principal(){
+  # Carrega variáveis do zsh
+  AMBIENTE="${HOME}/.zshenv"
+  [ -f "${AMBIENTE}" ] && source "${AMBIENTE}"
+  
+  ! [ -v NOME ] || ! [ -v SOBRENOME ] && obtem_nome_sobrenome
+  
+  var_desejadas=(ANIMAL
+    DISCIPLINA
+    LAB
+    PC
+  )  
+
+  for v in "${var_desejadas[@]}"; do
+    if ! [ -v "${v}" ]; then
+      print -n "Por gentileza, defina um valor para ${v}: "
+      read novo_valor
+
+      var_dysconta[${v}]=${novo_valor}
+    fi
+  done
+
+  touch "${AMBIENTE}"
+  for chave in ${(k)var_dysconta}
+    if ! grep -E "^${chave}=" "${AMBIENTE}"; then
+      echo ${chave}=${var_dysconta[$chave]} >> "${AMBIENTE}"
+    else
+      sed -i -e "s/^${chave}=.*/${chave}=${var_dysconta[$chave]}/" "${AMBIENTE}"
+    fi
+
+}
+
+principal
